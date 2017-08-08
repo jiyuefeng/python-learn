1d93b7de5a5460c354f1dbf9b2e5ca08acc05295
谢佩龙 <xiepeilong@corp.netease.com>
2017-08-08 10:10:29 +0800

    测试语法

---
+++ hello.py
@@ -0,0 +1,63 @@
+print(' hello perrone')
+#print('hello','perrone','and','小小爱哦利率')
+#print('1+30=',1+30)
+#name = input()
+#print('你输入了',name)
+
+#name1 = input('你的输入点什么？')
+#print('你输入了',name1)
+
+# 这是注释的
+
+#print('这是错误的,大小写敏感',Name1)
+
+# a = input('输入一个数字')
+# if a > 10 :
+# 	print('a大于10')
+# else :
+# 	print('a小鱼10')	
+
+print('i\'m perrone \"tell me\"')
+print('\\来转移\t什么\\')
+print(r'\\来转移\t什么\\')
+
+# test multi line
+
+print('''第一行
+第二行
+第三行''')
+
+
+# test bool
+print(3>2)
+print(3<2)
+
+# test and or not 
+print(3>2 and 3>1)
+print(3>2 or 3>1)
+print(not 3>2)
+
+a = 123
+print(a)
+a = 'anc'
+print(a)
+
+x = 1
+x = x + 10
+print(x)
+
+b = a
+
+a = 'xyz'
+print(a)
+print(b)
+
+PI = 3.14
+
+print(PI)
+
+print(10 / 3)
+print('浮点数',9 / 3)
+print(10 // 3)
+
+print(10 % 3)
